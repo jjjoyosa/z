f6e83f93fc386ac4dae7ebbc30e28b419166c8ab
Jenzzo Joyosa <125535579+jjjoyosa@users.noreply.github.com>
2024-05-20 03:45:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Define different speeds and dimensions for mobile
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-let move_speed = isMobile ? 6 : 2; // Increased move speed for mobile
-let gravity = isMobile ? 1 : 0.2; // Increased gravity for mobile
-let bird_flap = isMobile ? -16 : -6; // Increased flap speed for mobile
-let pipe_separation_interval = isMobile ? 40 : 170;
+let move_speed = isMobile ? 7 : 2; // Increased move speed for mobile
+let gravity = isMobile ? 0.9 : 0.2; // Increased gravity for mobile
+let bird_flap = isMobile ? -15 : -6; // Increased flap speed for mobile
+let pipe_separation_interval = isMobile ? 45 : 170;
 let pipe_gap = isMobile ? 35 : 35; // Increased pipe gap for better visibility
 let pipe_width = isMobile ? 70 : 80; // Increased pipe width for better interaction
 
@@ -24,6 +24,8 @@ let message = document.querySelector('.message');
 let score_title = document.querySelector('.score_title');
 
 let game_state = 'Start';
+img.style.width = isMobile ? '45px' : '50px'; // Reduce bird image size for mobile
+message.classList.add('messageStyle');
 
 
 document.addEventListener('keydown', (e) => {
@@ -45,8 +47,6 @@ function startGame() {
     img.style.display = 'block';
     bird.style.top = isMobile ? '30vh' : '40vh';
     game_state = 'Play';
-    img.style.width = isMobile ? '45px' : '50px'; // Reduce bird image size for mobile
-    message.classList.add('messageStyle');
     message.innerHTML = '';
     score_title.innerHTML = 'Score : ';
     score_val.innerHTML = '0';
